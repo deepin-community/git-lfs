629bf2adb080983965a3398431b6e0e0240b64bc
李成刚 <lichenggang@uniontech.com>
2023-12-01 16:16:30 +0800

    feat: update git-lfs to 3.4.0

---
+++ ssh/connection.go
@@ -0,0 +1,149 @@
+package ssh
+
+import (
+	"sync"
+
+	"github.com/git-lfs/git-lfs/v3/config"
+	"github.com/git-lfs/git-lfs/v3/subprocess"
+	"github.com/git-lfs/pktline"
+	"github.com/rubyist/tracerx"
+)
+
+type SSHTransfer struct {
+	lock         *sync.RWMutex
+	conn         []*PktlineConnection
+	osEnv        config.Environment
+	gitEnv       config.Environment
+	meta         *SSHMetadata
+	operation    string
+	multiplexing bool
+}
+
+func NewSSHTransfer(osEnv config.Environment, gitEnv config.Environment, meta *SSHMetadata, operation string) (*SSHTransfer, error) {
+	conn, multiplexing, err := startConnection(0, osEnv, gitEnv, meta, operation)
+	if err != nil {
+		return nil, err
+	}
+	return &SSHTransfer{
+		lock:         &sync.RWMutex{},
+		osEnv:        osEnv,
+		gitEnv:       gitEnv,
+		meta:         meta,
+		operation:    operation,
+		multiplexing: multiplexing,
+		conn:         []*PktlineConnection{conn},
+	}, nil
+}
+
+func startConnection(id int, osEnv config.Environment, gitEnv config.Environment, meta *SSHMetadata, operation string) (*PktlineConnection, bool, error) {
+	tracerx.Printf("spawning pure SSH connection")
+	exe, args, multiplexing := GetLFSExeAndArgs(osEnv, gitEnv, meta, "git-lfs-transfer", operation, true)
+	cmd, err := subprocess.ExecCommand(exe, args...)
+	if err != nil {
+		return nil, false, err
+	}
+	r, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, false, err
+	}
+	w, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, false, err
+	}
+	err = cmd.Start()
+	if err != nil {
+		return nil, false, err
+	}
+
+	var pl Pktline
+	if osEnv.Bool("GIT_TRACE_PACKET", false) {
+		pl = &TraceablePktline{id: id, pl: pktline.NewPktline(r, w)}
+	} else {
+		pl = pktline.NewPktline(r, w)
+	}
+	conn := &PktlineConnection{
+		cmd: cmd,
+		pl:  pl,
+		r:   r,
+		w:   w,
+	}
+	err = conn.Start()
+	if err != nil {
+		r.Close()
+		w.Close()
+		cmd.Wait()
+	}
+	tracerx.Printf("pure SSH connection successful")
+	return conn, multiplexing, err
+}
+
+// Connection returns the nth connection (starting from 0) in this transfer
+// instance or nil if there is no such item.
+func (tr *SSHTransfer) IsMultiplexingEnabled() bool {
+	return tr.multiplexing
+}
+
+// Connection returns the nth connection (starting from 0) in this transfer
+// instance or nil if there is no such item.
+func (tr *SSHTransfer) Connection(n int) *PktlineConnection {
+	tr.lock.RLock()
+	defer tr.lock.RUnlock()
+	if n >= len(tr.conn) {
+		return nil
+	}
+	return tr.conn[n]
+}
+
+// ConnectionCount returns the number of connections this object has.
+func (tr *SSHTransfer) ConnectionCount() int {
+	tr.lock.RLock()
+	defer tr.lock.RUnlock()
+	return len(tr.conn)
+}
+
+// SetConnectionCount sets the number of connections to the specified number.
+func (tr *SSHTransfer) SetConnectionCount(n int) error {
+	tr.lock.Lock()
+	defer tr.lock.Unlock()
+	return tr.setConnectionCount(n)
+}
+
+// SetConnectionCountAtLeast sets the number of connections to be not less than
+// the specified number.
+func (tr *SSHTransfer) SetConnectionCountAtLeast(n int) error {
+	tr.lock.Lock()
+	defer tr.lock.Unlock()
+	count := len(tr.conn)
+	if n <= count {
+		return nil
+	}
+	return tr.setConnectionCount(n)
+}
+
+func (tr *SSHTransfer) setConnectionCount(n int) error {
+	count := len(tr.conn)
+	if n < count {
+		for _, item := range tr.conn[n:count] {
+			tracerx.Printf("terminating pure SSH connection (%d -> %d)", count, n)
+			if err := item.End(); err != nil {
+				return err
+			}
+		}
+		tr.conn = tr.conn[0:n]
+	} else if n > count {
+		for i := count; i < n; i++ {
+			conn, _, err := startConnection(i, tr.osEnv, tr.gitEnv, tr.meta, tr.operation)
+			if err != nil {
+				tracerx.Printf("failed to spawn pure SSH connection: %s", err)
+				return err
+			}
+			tr.conn = append(tr.conn, conn)
+		}
+	}
+	return nil
+}
+
+func (tr *SSHTransfer) Shutdown() error {
+	tracerx.Printf("shutting down pure SSH connection")
+	return tr.SetConnectionCount(0)
+}
---
+++ t/cmd/lfs-ssh-echo.go
@@ -0,0 +1,115 @@
+//go:build testtools
+// +build testtools
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
+)
+
+type sshResponse struct {
+	Href      string            `json:"href"`
+	Header    map[string]string `json:"header"`
+	ExpiresAt time.Time         `json:"expires_at,omitempty"`
+	ExpiresIn int               `json:"expires_in,omitempty"`
+}
+
+func shell() string {
+	if runtime.GOOS == "windows" {
+		return "bash"
+	}
+	return "sh"
+}
+
+func spawnCommand(command string) error {
+	cmd := exec.Command(shell(), "-c", command)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok {
+		var ws syscall.WaitStatus
+		ws, ok = e.ProcessState.Sys().(syscall.WaitStatus)
+		if ok {
+			os.Exit(ws.ExitStatus())
+		}
+	}
+	return err
+}
+
+func checkSufficientArgs(offset int) {
+	if len(os.Args) < offset+2 {
+		fmt.Fprintf(os.Stderr, "got %d args: %v", len(os.Args), os.Args)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// expect args:
+	//   lfs-ssh-echo [-p PORT [--]] git@127.0.0.1 "git-lfs-authenticate REPO OPERATION"
+	//   lfs-ssh-echo [-p PORT [--]] git@127.0.0.1 "git-lfs-transfer REPO OPERATION"
+	//   lfs-ssh-echo git@127.0.0.1 "git-upload-pack REPO"
+	//   lfs-ssh-echo git@127.0.0.1 "git-receive-pack REPO"
+	offset := 1
+
+	checkSufficientArgs(offset)
+	if os.Args[offset] == "-oControlMaster=auto" {
+		offset += 2
+	}
+
+	checkSufficientArgs(offset)
+	if os.Args[offset] == "-p" {
+		offset += 2
+	}
+
+	checkSufficientArgs(offset)
+	if os.Args[offset] == "--" {
+		offset += 1
+	}
+
+	checkSufficientArgs(offset)
+	if os.Args[offset] != "git@127.0.0.1" {
+		fmt.Fprintf(os.Stderr, "expected \"git@127.0.0.1\", got %q", os.Args[offset])
+		os.Exit(1)
+	}
+
+	// just "git-lfs-(authenticate|transfer) REPO OPERATION" or "git-(upload|receive)-pack REPO"
+	remoteCmd := strings.Split(os.Args[offset+1], " ")
+	if len(remoteCmd) < 2 {
+		fmt.Fprintf(os.Stderr, "bad command line: %s\nargs: %v", remoteCmd, os.Args)
+		os.Exit(1)
+	}
+
+	if remoteCmd[0] == "git-lfs-transfer" || remoteCmd[0] == "git-upload-pack" || remoteCmd[0] == "git-receive-pack" {
+		err := spawnCommand(os.Args[offset+1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error running command %q: %v", remoteCmd[0], err)
+			os.Exit(1)
+		}
+		return
+	}
+
+	repo := remoteCmd[1]
+
+	r := &sshResponse{
+		Href: fmt.Sprintf("http://127.0.0.1:%s/%s.git/info/lfs", os.Args[2], repo),
+	}
+	switch repo {
+	case "/ssh-expired-absolute":
+		r.ExpiresAt = time.Now().Add(-5 * time.Minute)
+	case "/ssh-expired-relative":
+		r.ExpiresIn = -5
+	case "/ssh-expired-both":
+		r.ExpiresAt = time.Now().Add(-5 * time.Minute)
+		r.ExpiresIn = -5
+	}
+
+	json.NewEncoder(os.Stdout).Encode(r)
+}
